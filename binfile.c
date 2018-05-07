9937f5f653daa2f6a2dd3ba75c89066b9606ae97
Jennifer King <32186812+mairodia@users.noreply.github.com>
2018-05-06 17:42:42 -0700

    Update binfile.c

--- binfile.c
+++ binfile.c
@@ -47,39 +47,52 @@ void    WriteRecs(const PhoneRoom  recs[], int  numRecs, FILE*  fPtr);
 
 int     main(void)
 {
-    int                 counter;
-    PhoneRoom   myRecs;
+int     main(void)
+{
+    auto        unsigned        int             numRecs;
+    auto        char            buf[BUFLEN];
+    auto        FILE*           inFile;
+    auto        FILE*           outFile;
+    PhoneRoom                   *records;
 
     // open the input file that contains the binary records
-    FILE *myFile;
-    myFile = fopen(DAT_FNAME, "rb");
-    if(!myFile)
+    inFile = fopen(DAT_FNAME, "r");
+    if(fseek(inFile, 0, SEEK_END))
     {
         puts("Unable to open file!");
-        return 1;
+        exit(EXIT_FAILURE);
     }
     // calculate the number of records stored in the file and display the total
     // to stdout
-    for(counter = 1; counter <= BUFLEN; counter++)
-    {
-        myRecs.phone = counter;
-        fwrite(&myRecs, sizeof(struct PhoneRoom), 1, myFile);
-    }
+    numRecs = ftell(inFile)/sizeof(PhoneRoom);
 
     // allocate an array of records large enough to hold all of the data
-
+    records = malloc(numRecs * sizeof(PhoneRoom));
+    if(records == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
 
     // read all the file records into the allocated buffer
-
+    rewind(inFile);
+    printf("There are %d records.\n", numRecs);
+    if(numRecs != fread(records, sizeof(PhoneRoom), numRecs, inFile))
+    {
+        exit(EXIT_FAILURE);
+    }
 
     // get the name of the output file from the user and open it
-
+    printf("Enter the name of the output file: ");
+    fgets(buf, BUFLEN, stdin);
+    outFile = fopen(buf, "w");
 
     // call WriteRecs to write out the records in formatted text
-
+    WriteRecs(records, numRecs, outFile);
 
     // close the files and release allocated memory
-    fclose(myFile);
+    fclose(inFile);
+    fclose(outFile);
+    free(records);
 
     return 0;
 
@@ -109,6 +122,9 @@ int     main(void)
 
 void    WriteRecs(const PhoneRoom  recs[], int  numRecs, FILE*  fPtr)
 {
-    ???
+    for(int index = 0; index < numRecs; index++)
+    {
+        fprintf(fPtr, "Phone: %d-%d Room #: %d\n", recs[index].phone / 10000, recs[index].phone % 1000, recs[index].room);
+    }
 
 }  // end of "WriteRecs"
